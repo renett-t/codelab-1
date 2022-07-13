d3c5892f781300af2ee20d463e86e2a4cf2a4f07
renett_t <rtapkina@gmail.com>
2022-07-14 00:20:14 +0300

    Task 6: Added Log statement

--- app/src/main/java/ru/renett/helloworld/MainActivity.kt
+++ app/src/main/java/ru/renett/helloworld/MainActivity.kt
@@ -1,11 +1,17 @@
 package ru.renett.helloworld
 
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import android.util.Log
+
+private val LOG_TAG = MainActivity::class.simpleName
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        Log.d(LOG_TAG, "Hello World")
     }
 }
